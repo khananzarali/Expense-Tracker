894e8da70f22d616afb746c087812317f8fa7ed2
AnzarAliKhan <khananzarali7@gmail.com>
2026-07-15 14:27:19 +0530

    Login Update 2

---
+++ Frontend/components/Login.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+function Login(){
+    const [username,setusername]=useState("");
+    const [password,setpassword]=useState("");
+    const handlesumbit=async(e)=>{
+       e.preventDefault();
+       try {
+        await axios.post("https",{
+            username,
+            password
+        })
+        localStorage.setItem("loggedInItem","true")
+        Navigate("/home")
+       } catch (error) {
+        
+       }
+
+    }
+    return(
+        <>
+        <form type="submit">
+            <input type="text" value={username} onChange={(e)=>setusername(e.traget.value)}/>
+            <input type="password" value={password} onChange={(e)=>setpassword(e.traget.value)}/>
+            <button type="submit" >Submit</button>
+        </form>
+        </>
+    )
+}
+export default Login
